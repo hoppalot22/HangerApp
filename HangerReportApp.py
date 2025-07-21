2fc39407e29cb46d7b40ebfba8b9bf5699fdf546
Hoppalot22 <alex.michael.horner@gmail.com>
2025-07-22 09:08:09 +1000

    h

--- HangerReportApp.py
+++ HangerReportApp.py
@@ -19,9 +19,13 @@ class MainWindow():
         photoProcessTab = ImageProcessTab(self.tabControl)
         newJobTab = JobPrepareTab(self.tabControl)
 
+        #root.bind("<Configure>", photoProcessTab.gui.OnResize)
+
         self.tabControl.add(reportGen, text = "Report Generation")
         self.tabControl.add(photoProcessTab, text = "Image Processing")
         self.tabControl.add(newJobTab, text = "Prepare for Job")
+
+        
         
         self.tabControl.pack(expand=1, fill = "both")
 
@@ -85,7 +89,7 @@ class JobPrepareTab(tk.Frame):
         pass
 
 class ImageProcessTab(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent : ttk.Frame):
         super().__init__(parent)
         self.parent = parent
         self.rootFolderPath = None
@@ -107,8 +111,8 @@ class ImageProcessTab(tk.Frame):
         self.photoButtons.bind("<<Right>>", lambda e: self.PhotoNav(1))
 
         self.treeView = DirectoryTree(self)
-        self.treeView.bind("<<TreeViewSelect>>", self.TreeUpdate)
-        self.treeView.pack()
+        self.treeView.bind("<<NodeSelect>>", self.TreeUpdate)
+        self.treeView.pack(expand=True)
 
         processColumn = EntityColumn(self)
         processColumn.AddButton("Select Folder", text="Select Root Folder", command = self.SelectDirectory)
@@ -119,7 +123,7 @@ class ImageProcessTab(tk.Frame):
         processColumn.AddCheckBox(self, "Perspective", text = "Perspective Correction", variable=self.gui.perspectiveOverlay, command = self.gui.UpdateScreen)
         processColumn.pack()
 
-        statusLabel = tk.Label(self, text = self.statusText)
+        statusLabel = tk.Label(self, text = self.statusText, wraplength = 100, justify = "center")
         statusLabel.pack(side = 'bottom')
 
         self.statusLabel = statusLabel
@@ -144,12 +148,14 @@ class ImageProcessTab(tk.Frame):
 
     def SelectDirectory(self):
         self.rootFolderPath = filedialog.askdirectory()
-        self.treeView.SelectFolder(self.rootFolderPath)
+        self.treeView.LoadTree(self.rootFolderPath)
+        self.treeView.tree.focus(self.treeView.leaves[0])
+        self.treeView.SelectNode("pass")
         myThead = threading.Thread(target=self.getPictures)
         myThead.start()
 
     def UpdateLabel(self):
-        self.statusLabel.configure(text = self.statusText)
+        self.statusLabel.configure(text = self.statusText, wraplength = self.statusLabel.master.winfo_width())
 
     def UpdatePicture(self):
         if(os.path.isfile(self.treeView.selectionPath)) and self.treeView.selectionPath.split(".")[-1].lower() in ["jpg", "png", "jpeg"]:
@@ -160,7 +166,7 @@ class ImageProcessTab(tk.Frame):
         self.UpdatePicture()
 
     def TreeUpdate(self, event):
-        self.statusText = f"Image: {self.treeView.selectionPath}"
+        self.statusText = f"Image: {self.treeView.tree.item(self.treeView.currentLeaf)["text"]}"
         self.Update(event)
 
     def HangerNav(self, inc):
@@ -192,13 +198,12 @@ class PrevNextButtonUI(tk.Frame):
 class EntityColumn(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        self.entities = {}
-        
+        self.entities = {}        
         
     def AddButton(self, name, **kwargs):
         button = tk.Button(self, **kwargs)
-        self.entities[name] = button
         button.pack()
+        self.AddToEntityList(button, name)
 
     def AddField(self, name, text = "Unnamed"):
         frame = ttk.Frame(self)
@@ -208,33 +213,50 @@ class EntityColumn(tk.Frame):
         label.pack(side = "left")
         self.entities[name] = entryBox
         frame.pack(side = "top", anchor="nw", expand = True)
+        self.AddToEntityList(entryBox, name)
 
     def AddCheckBox(self, parent, name, **kwargs):
         checkBox = tk.Checkbutton(parent, **kwargs)
         self.entities[name] = checkBox
         checkBox.pack()
+        self.AddToEntityList(checkBox, name)
+
+    def AddToEntityList(self, myObject, name):
+        if name in self.entities.values():
+            object.destroy()
+            raise("Entity name already in use in this column")
+        else:
+            self.entities[name] = myObject
 
 class ProjectTree(tk.Frame):
     def __init__(self, parent, projName):
         super().__init__(parent)
         self.projName = projName
 
-        self.nodes = dict()
+        self.nodes = []
         self.tree = ttk.Treeview(self)
         ysb = ttk.Scrollbar(self, orient='vertical', command=self.tree.yview)
         xsb = ttk.Scrollbar(self, orient='horizontal', command=self.tree.xview)
         self.tree.configure(yscroll=ysb.set, xscroll=xsb.set)
         self.tree.heading('#0', text='Project tree', anchor='w')
+        self.tree.column("#0", stretch=False)
+
+        self.editBox = None
 
         self.tree.grid()
         ysb.grid(row=0, column=1, sticky='ns')
         xsb.grid(row=1, column=0, sticky='ew')
 
-        self.tree.bind('<<TreeviewSelect>>', self.SelectNode)
+        self.tree.bind('<<TreeviewSelect>>', self.SelectNode)        
+        self.tree.bind("<Double-1>", self.EditNode)
+        self.tree.bind("<F2>", self.EditNode)
 
         self.treeContols = EntityColumn(self)
         self.treeContols.AddField("Amount", text = "No. nodes to add")
         self.treeContols.AddButton("Add", text = "Add", command = self.AddNodes)
+        self.treeContols.AddButton("Delete", text = "Delete", command = self.DeleteNode)
+
+        self.treeContols.entities["Amount"].bind("<Return>", lambda e : self.AddNodes())
 
         self.treeContols.grid(row = 2, column = 0)
 
@@ -243,32 +265,59 @@ class ProjectTree(tk.Frame):
             self.AddNode()
 
     def AddNode(self, name = "New Node"):
-        currentNode = self.tree.focus()
-        currentSiblings = list(self.tree.get_children(self.tree.parent(currentNode)))        
+        currentNode = self.tree.focus()      
         counter = 0
         newName = name
-        while newName in [self.tree.item(i)["text"] for i in currentSiblings]:
+        while newName in [self.tree.item(i)["text"] for i in self.tree.get_children(currentNode)]:
             counter += 1
             newName = name + f"({counter})"
-        self.tree.insert(currentNode, 'end', text = newName)
+        newNode = self.tree.insert(currentNode, 'end', text = newName)
+        self.nodes.append(newNode)
+
+    def DeleteNode(self):
+        self.tree.delete(self.tree.focus())
+
+    def EditNode(self, event):
+        for node in self.nodes:
+            if ((self.tree.bbox(node)[0] < event.x) and (self.tree.bbox(node)[0] + self.tree.bbox(node)[2] > event.x) and (self.tree.bbox(node)[1] < event.y) and (self.tree.bbox(node)[1] + self.tree.bbox(node)[3] > event.y)):
+                text = tk.StringVar(value = self.tree.item(node, "text"))
+                self.editNode = node
+                self.editBox = tk.Entry(self, textvariable = text, width = self.tree.bbox(node)[2])
+                self.editBox.place(x = self.tree.bbox(node)[0], y = self.tree.bbox(node)[1])
+
+                self.editBox.bind("<FocusOut>", self.FinishEdit)
+                self.editBox.bind("<Return>", self.FinishEdit)
+                self.editBox.bind("<Escape>", self.FinishEdit)
+                self.editBox.bind("<FocusIn>", self.Highlight)
+
+                self.editBox.focus()
+
+    def Highlight(self, event):
+        self.editBox.selection_range(0, tk.END)
+
+    def FinishEdit(self, event : tk.Event):     
+        if not (event.keysym == "Escape"):
+            self.tree.item(self.editNode, text = self.editBox.get())
+        self.editBox.destroy()
 
     def SelectNode(self, event):
         pass
-
-    
 
 class DirectoryTree(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.nodes = dict()
         self.tree = ttk.Treeview(self)
+        self.leaves = []
         ysb = ttk.Scrollbar(self, orient='vertical', command=self.tree.yview)
         xsb = ttk.Scrollbar(self, orient='horizontal', command=self.tree.xview)
-        self.tree.configure(yscroll=ysb.set, xscroll=xsb.set)
+        self.tree.configure(yscrollcommand=ysb.set, xscrollcommand=xsb.set)
         self.tree.heading('#0', text='Project tree', anchor='w')
+        self.tree.column("#0", stretch=False)
 
         self.currentSiblings = []
         self.currentSibling = 0
+        self.currentLeaf = ''
 
         self.item = None
         self.selectionPath = ""
@@ -277,82 +326,55 @@ class DirectoryTree(tk.Frame):
         ysb.grid(row=0, column=1, sticky='ns')
         xsb.grid(row=1, column=0, sticky='ew')
 
-        self.tree.bind('<<TreeviewOpen>>', self.open_node)
         self.tree.bind('<<TreeviewSelect>>', self.SelectNode)
 
-        
-    def SelectFolder(self, path):  
-        for i in self.tree.get_children():
-            self.tree.delete(i)
-        abspath = os.path.abspath(path)
-        self.insert_node('', abspath, abspath)
+
+    def LoadTree(self, rootPath, node = ''):
+        self.tree.column("#0", width=1800, stretch=False)
+        for item in os.listdir(rootPath):
+            newNode = self.tree.insert(node, 'end', text = item, values=[rootPath + "\\" + item])
+            if os.path.isdir(rootPath + "\\" + item):
+                self.LoadTree(rootPath + "\\" + item, node = newNode)
+            else:
+                self.leaves.append(newNode)
+
 
     def SelectNode(self, event):
         self.focus = self.tree.focus()
         self.item = self.tree.item(self.focus)
         self.currentSiblings = list(self.tree.get_children(self.tree.parent(self.focus)))
-        self.currentSibling = self.currentSiblings.index(self.focus)   
+        self.currentSibling = self.currentSiblings.index(self.focus)
+        
 
-        path = [self.item["text"]]
-        focus = self.focus
-        for i in range(50):
-            parent = self.tree.parent(focus)
-            text = self.tree.item(parent)["text"]
-            if text != "":
-                path.append(text)
-            else:
-                break
-            focus = parent
-        else:
-            print("Warning, tree depth exceeded")
-        path = "\\".join(list(reversed(path)))
-        self.selectionPath = path
-        if(os.path.isfile(path)):
-            self.event_generate("<<TreeViewSelect>>")
+        if(self.focus in self.leaves):
+            self.currentLeaf = self.focus
+            self.selectionPath = self.item["values"][0]
 
-    def insert_node(self, parent, text, abspath):
-        node = self.tree.insert(parent, 'end', text=text, open=False)
-        if os.path.isdir(abspath):
-            self.nodes[node] = abspath
-            self.tree.insert(node, 'end')
-
-    def open_node(self, event):
-        node = self.tree.focus()
-        abspath = self.nodes.pop(node, None)
-        if abspath:
-            self.tree.delete(self.tree.get_children(node))
-            for p in os.listdir(abspath):
-                self.insert_node(node, p, os.path.join(abspath, p))
+            if(os.path.isfile(self.selectionPath)):
+                self.event_generate("<<NodeSelect>>")
 
     def SelectNext(self, inc):
         self.currentSibling += inc
-        for i in range(50):
-            if(self.tree.item(self.currentSiblings[self.currentSibling%len(self.currentSiblings)])["text"].split(".")[-1].lower() not in ["jpg", "png", "jpeg"]):
+        for i in range(len(self.currentSiblings)):
+            if(self.tree.item(self.currentSiblings[self.currentSibling%len(self.currentSiblings)])["values"][0].split(".")[-1].lower() not in ["jpg", "png", "jpeg"]):
                 self.currentSibling += inc
             else:
                 break
         self.tree.focus(self.currentSiblings[self.currentSibling%len(self.currentSiblings)])
-        self.SelectNode(inc)
-    
-    
-#Need to account for parents being both images and folders
+        self.SelectNode("pass")
 
     def SelectNextParent(self, inc):
-        self.currentSibling = 0
-        currentParents = list(self.tree.get_children(self.tree.parent(self.tree.parent(self.focus))))
-        currentParent = currentParents.index(self.tree.parent(self.focus))+inc
-        print(currentParent)
-        print(currentParents)
 
-        for i in range(1000):
-            if not(os.path.isdir(f"{self.tree.item(currentParents[currentParent])["text"]}")):
-                currentParent+=inc
-            else:
-                break
+        if self.currentLeaf in self.leaves:
+            currentLeaf = self.leaves.index(self.currentLeaf)
+        else:
+            currentLeaf = 0
 
-        self.tree.focus(self.tree.get_children(currentParents[(currentParent+inc)%len(currentParents)])[0])
-        print(self.tree.item(self.tree.get_children(currentParents[(currentParent+inc)%len(currentParents)])[0]))
-        self.SelectNode(inc)
+        while self.tree.parent(self.leaves[currentLeaf]) == self.tree.parent(self.leaves[(currentLeaf+inc)%len(self.leaves)]):
+            inc+=inc
+        newLeaf = (currentLeaf+inc)%len(self.leaves)
+        self.tree.focus(self.leaves[newLeaf])     
+        self.SelectNode("pass")
     
 class Project():
     def __init__(self, name):
@@ -367,7 +389,6 @@ class Project():
     def AddComponent(self):
         self.componentIDs.append(self.nextID)
         self.nextID += 1
-
 
 def Main():
     myWindow = MainWindow()
